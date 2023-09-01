77e0b1203feb24b3da345f6f197874584a2b71ee
wzrdk3lly <dkmk1149@gmail.com>
2023-08-31 22:47:23 -0400

    ADD from and to address input fields

--- src/App.jsx
+++ src/App.jsx
@@ -10,20 +10,23 @@ function App() {
   - splits contract address = 0xC97c3Ad1Cd7160Dc5939068a344A1850c44eb27B
   */
 
-    const [walletAddress, setWalletAddress] = useState("0x81215d34367AF48d01E728AfF2976d9Df32fE604");
+  const [walletAddress, setWalletAddress] = useState("");
+  const [toAddress, setToAddress] = useState("");
+  const [fromAddress, setFromAddress] = useState("")
+
   // getConnected wallet address and set the state of connectedWallet
 
   // getSplitHistory async function using current connected wallet address 
 
-  // performSplit async function 
-
+   async function performSplit(){
+    console.log("performing split")
+}
   function connectWallet(){
     console.log("wallet connect attempt")
   }
 
-
   return (
-   <div className='bg-slate-800 h-screen'>
+   <div >
     {/* Header component with the splits logo */}
      <Header
       walletAddress={walletAddress}
@@ -31,17 +34,31 @@ function App() {
       />
 
      {/* Main Card with */}
-     {/* Header 3 to call out splits  */}
-     {/* Paragraph with a sentence on what the user should do  */}
-     {/* Start form with a from label  */}
-     {/* input text field that highlights white when clicked into  */}
+     <div className='flex justify-center items-center h-screen text-white'>
+      <div className='border w-[40%] p-4'>
+        <h1 className='text-2xl font-bold'>Perform Split</h1>
+        <span>Send half of your balance to the "To Address" </span>
+        <form className="mt-2" action="">
+          <div className='grid grid-cols-1 gap-y-2'>
+          <label htmlFor="fromAddress">From Address</label>
+          <input className="bg-slate-400" type="text" name='fromAddress' value="" readOnly/>
+          <label htmlFor="toAddress">To Address</label>
+          <input className="bg-slate-400" type="toAddress" name='toAddress' value={toAddress} onChange={(e) => setToAddress(e.target.value)} />
+          </div>
+        </form>
+        <div className='mt-4'>Status: </div>
+        <div className='mt-4'>Split History: </div>
+        
+      
+        <div className='flex justify-center mt-4'>
+        <button className="border h-8 w-36" onClick={performSplit}>Split</button>
+        </div>
+       
+      </div>
+  
 
-     {/* to label */}
-     {/* input test field that highlights white when clicked or selected */}
+     </div>
 
-     {/* Status label: will emit an event if split is successful */}
-
-     {/* solit history label: displays current split history of from address  */}
     
      
 
