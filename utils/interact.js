116fe2baab9bc70b079369abbaf0257d1bd5affa
wzrdk3lly <dkmk1149@gmail.com>
2023-09-07 18:58:05 -0500

    UPDATE change env type

--- utils/interact.js
+++ utils/interact.js
@@ -2,20 +2,20 @@
 import { Network, Alchemy } from "alchemy-sdk";
 
 import { ethers, formatEther } from "ethers";
-import dotenv from "dotenv";
+// import dotenv from "dotenv";
 
-dotenv.config();
+// dotenv.config();
 
 // config alchemy provider
 
-// const settings = {
-//   apiKey: import.meta.VITE_ALCHEMY_API_KEY,
-//   network: Network.ETH_GOERLI,
-// };
 const settings = {
-  apiKey: process.env.VITE_ALCHEMY_API_KEY,
+  apiKey: import.meta.VITE_ALCHEMY_API_KEY,
   network: Network.ETH_GOERLI,
 };
+// const settings = {
+//   apiKey: process.env.VITE_ALCHEMY_API_KEY,
+//   network: Network.ETH_GOERLI,
+// };
 
 const alchemy = new Alchemy(settings);
 const provider = await alchemy.config.getProvider();
